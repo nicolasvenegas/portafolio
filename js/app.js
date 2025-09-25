c971a9e0166eda1da8ccda06bc7b786811fd2d28
Nico <96893196+nicolasvenegas@users.noreply.github.com>
2025-09-25 09:38:49 -0300

    link av-visual

--- js/app.js
+++ js/app.js
@@ -1,25 +1,21 @@
 $(document).foundation();
 
-
-
 $(document).ready(function () {
-    $('[data-curtain-menu-button]').click(function () {
-        $('body').toggleClass('curtain-menu-open');
-    })
+  $('[data-curtain-menu-button]').click(function () {
+    $('body').toggleClass('curtain-menu-open');
+  })
 });
 
 
-
-    const lenis = new Lenis()
-    lenis.on('scroll', (e) => {
-      console.log(e)
-    })
-    function raf(time) {
-      lenis.raf(time)
-      requestAnimationFrame(raf)
-    }
-    requestAnimationFrame(raf)
-
+const lenis = new Lenis()
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+requestAnimationFrame(raf)
 
 
-    
+
